228d103970bbfc0f0d7dfc0576fb523489b0f23d
Max Kelly <max@revelthat.com>
2019-08-15 16:58:04 +1000

    Exploring JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,10 +4,56 @@ console.log("App.js is running");
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h2",
-  null,
-  "YES this did change"
+    "div",
+    null,
+    React.createElement(
+        "h2",
+        null,
+        " My React Site "
+    ),
+    React.createElement(
+        "p",
+        null,
+        " This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            " Item One "
+        ),
+        React.createElement(
+            "li",
+            null,
+            " Item Two "
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        " Max Kelly "
+    ),
+    React.createElement(
+        "p",
+        null,
+        " Age: 21 "
+    ),
+    React.createElement(
+        "p",
+        null,
+        " Location: Melbourne, Victoria, Australia "
+    )
+);
+
 var appRoot = document.getElementById("app");
+var profileRoot = document.getElementById("profile");
 
 ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, profileRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,28 @@
 console.log("App.js is running");
 
 // JSX - JavaScript XML
-var template = <h2>YES this did change</h2>
+var template = (
+  <div>
+    <h2> My React Site </h2> 
+    <p> This is some info</p>
+    <ol>
+        <li> Item One </li>
+        <li> Item Two </li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1> Max Kelly </h1>
+        <p> Age: 21 </p>
+        <p> Location: Melbourne, Victoria, Australia </p>
+
+    </div>
+);
+
 var appRoot = document.getElementById("app");
+var profileRoot = document.getElementById("profile")
 
 ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, profileRoot)
