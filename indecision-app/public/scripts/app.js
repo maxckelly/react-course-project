c237c9509762c3861e87611355b64770eb0e420a
Max Kelly <max@revelthat.com>
2019-08-20 18:49:54 +1000

    17. Manual Data Binding

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,63 +1,112 @@
 'use strict';
 
-// Arguments object - no longer bound with arrow functions
+// JSX - JavaScript XML
+// For JSX class needs to be called className
 
-// The below function would not work
+// ---Template one---
 
-// const add = (a, b) => {
-//     console.log(arguments)
-//     return a + b
-// }
-
-// The below function would work. 
-
-var add = function add(a, b) {
-    console.log(arguments);
-    return a + b;
+var welcome = {
+    title: 'Welcome to my app',
+    subtitle: 'Its the indecision app, helping you make your decision',
+    options: ['One ', 'Two ']
 };
 
-console.log(add(55, 1, 101));
+var todos = {
+    todoOne: 'Walk the dogs'
+};
 
-// this keyword no longer bound with arrow functions
-// The below would not work due to the function word being used it needs to be an arrow function instead.
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h2',
+        null,
+        welcome.title
+    ),
+    welcome.subtitle && React.createElement(
+        'p',
+        null,
+        welcome.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        welcome.options.length > 0 ? 'Here are your options: ' + welcome.options : 'No options'
+    )
+);
+
+var count = 0;
+var addOne = function addOne() {
+    console.log('CLICKED!');
+};
+var minusOne = function minusOne() {
+    console.log('MinusOne');
+};
+var reset = function reset() {
+    console.log('Reset');
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Count: ',
+        count
+    ),
+    React.createElement(
+        'button',
+        { onClick: addOne },
+        '+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: reset },
+        'Reset'
+    )
+);
+
+// Challenge
+
+// Make a button with the text "-1"
+// Setup minusOne function and register onclick 
+// log minusOne everytime it gets clicked
+// Mae reset button "rest" - setup reset function to log reset everytime it gets clicked.
+
+console.log(templateTwo);
+var appRoot = document.getElementById("app");
+ReactDOM.render(templateTwo, appRoot);
+
+//-- Template two ---
 // const user = {
-//     name: 'Max',
-//     cities: ['Melbourne, Sydney, Perth, New York'],
-//     printPlacesLived: function() {
+//     name: '',
+//     age: 17,
+//     location: 'Melbourne'
+// };
 
-//         this.cities.forEach(function (city) {
-//             console.log(this.name + ' has lived in' + city)
-//         })
+// // Says if there is no location then the <p></p> doesn't show up
+// function getLocation(location) {
+//     if (location) {
+//         return <p>Location: {location}</p>
 //     }
 // }
 
-var userArrowFunction = {
-    name: "Max",
-    cities: ['Melbourne', 'Sydney', 'Perth', 'New York'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
+// const templateTwo = (
+//     <div>
+//         <h1>{user.name ? user.name : "Anonymous"}</h1>{" "}
+//         {/* The above uses a ternary operator basically saying if user.name exists then display user.name if not then display anonymous  */}
+//         {user.age && user.age >= 18 && <p>{`Age: ` + user.age}</p>}
+//         {/* The above: 1. Checks if user.age exists and then checks if user.age is above or equal to 18 and if so displays the <p></p> tag  */}
+//         {getLocation(user.location)}
+//         {/* The above get the getLocation function and then if all correct displays the user.location.  */}
+//     </div>
+// );
 
-        return this.cities.map(function (city) {
-            return _this.name + ' had lived in ' + city;
-        });
-    }
-};
-
-console.log(userArrowFunction.printPlacesLived());
-
-// Challenge area
-
-var multiplier = {
-
-    multiplyBy: 24,
-    numbers: [12, 20, 30, 50],
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
-    }
-};
-
-console.log(multiplier.multiply());
+// const profileRoot = document.getElementById("profile")
+// ReactDOM.render(templateTwo, profileRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,4 +1,5 @@
 // JSX - JavaScript XML
+// For JSX class needs to be called className
 
 // ---Template one---
 
@@ -20,33 +21,63 @@ const template = (
     </div>
 );
 
-// -- Template two ---
-const user = {
-    name: '',
-    age: 17,
-    location: 'Melbourne'
-};
 
-// Says if there is no location then the <p></p> doesn't show up
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>
-    }
+let count = 0
+const addOne = () => {
+    console.log('CLICKED!')
+}
+const minusOne = () => {
+    console.log('MinusOne')
+}
+const reset = () => {
+    console.log('Reset')
 }
 
 const templateTwo = (
     <div>
-        <h1>{user.name ? user.name : "Anonymous"}</h1>{" "}
-        {/* The above uses a ternary operator basically saying if user.name exists then display user.name if not then display anonymous  */}
-        {user.age && user.age >= 18 && <p>{`Age: ` + user.age}</p>}
-        {/* The above: 1. Checks if user.age exists and then checks if user.age is above or equal to 18 and if so displays the <p></p> tag  */}
-        {getLocation(user.location)}
-        {/* The above get the getLocation function and then if all correct displays the user.location.  */}
+        <h1>Count: {count}</h1>
+        <button onClick={addOne} >+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={reset}>Reset</button>
     </div>
 );
 
-const appRoot = document.getElementById("app");
-const profileRoot = document.getElementById("profile")
 
-ReactDOM.render(template, appRoot);
-ReactDOM.render(templateTwo, profileRoot)
+// Challenge
+
+// Make a button with the text "-1"
+// Setup minusOne function and register onclick 
+// log minusOne everytime it gets clicked
+// Mae reset button "rest" - setup reset function to log reset everytime it gets clicked.
+
+console.log(templateTwo)
+const appRoot = document.getElementById("app");
+ReactDOM.render(templateTwo, appRoot);
+
+//-- Template two ---
+// const user = {
+//     name: '',
+//     age: 17,
+//     location: 'Melbourne'
+// };
+
+// // Says if there is no location then the <p></p> doesn't show up
+// function getLocation(location) {
+//     if (location) {
+//         return <p>Location: {location}</p>
+//     }
+// }
+
+// const templateTwo = (
+//     <div>
+//         <h1>{user.name ? user.name : "Anonymous"}</h1>{" "}
+//         {/* The above uses a ternary operator basically saying if user.name exists then display user.name if not then display anonymous  */}
+//         {user.age && user.age >= 18 && <p>{`Age: ` + user.age}</p>}
+//         {/* The above: 1. Checks if user.age exists and then checks if user.age is above or equal to 18 and if so displays the <p></p> tag  */}
+//         {getLocation(user.location)}
+//         {/* The above get the getLocation function and then if all correct displays the user.location.  */}
+//     </div>
+// );
+
+// const profileRoot = document.getElementById("profile")
+// ReactDOM.render(templateTwo, profileRoot);
